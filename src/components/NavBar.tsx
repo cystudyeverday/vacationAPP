025e1cd26878a4ed23fe020da67b0db40055d9c3
caiyang <373805195@qq.com>
2022-03-22 23:18:08 +0800

    mainPage mock

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd';
-import NavBar from '../../components/NavBar'
-import ContentList from '../../components/ContentList'
-import './index.css'
 
 const { Header, Content, Footer } = Layout;
 
 const navs = ['最热地点', '最热攻略', '我的收藏', '我的帖子', '我的账号']
 
-const Home = () => {
+const NavBar = (props: any) => {
     return (
         <div className='home-page'>
-            <NavBar>
-                <ContentList />
-
-            </NavBar>
-            {/* <Layout className="layout">
+            <Layout className="layout">
                 <Header>
                     <div className="logo" />
                     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
@@ -37,9 +30,9 @@ const Home = () => {
                     {props.children}
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>Created in 2022</Footer>
-            </Layout>, */}
+            </Layout>,
         </div>
     )
 }
 
-export default Home
+export default NavBar
