a18f24206a218bd45e8223f3bec99f55c9e5297e
caiyang <373805195@qq.com>
2022-04-11 09:15:23 +0800

    feature: upload article

--- src/models/post.ts
+++ src/models/post.ts
@@ -2,14 +2,19 @@ import { createModel, init } from '@rematch/core'
 import { fromJS } from 'immutable'
 import { set as helpSet } from '../utils/reducers'
 import type { RootModel } from '.'
+import { message } from 'antd'
+import { requestWithToken } from '../utils/request'
+import axios from 'axios';
+import { authHeader } from "../api/helps";
 
 export const post = createModel<RootModel>()({
     state: fromJS({
         form: {
-            username: 'init',
-            description: 'init',
-            password: "***",
-
+            title: "",
+            brief: "",
+            article: "",
+            file: [],
+            label: ""
         }
     }),
     reducers: {
@@ -19,7 +24,64 @@ export const post = createModel<RootModel>()({
 
     },
     effects: (dispatch) => ({
+        // async postContent(payload) {
+        //     const requestOption = {
+        //         url: '/upload/uploadArticle',
+        //         method: 'POST',
+        //         endpoint: "http://8.130.19.187:8083",
+        //         payload
+
+        //     }
+        //     const { success, errorMessage } = await requestWithToken(requestOption)
+        //     if (success) {
+        //         // this.set(['articles', fromJS(data)])
+        //         message.success("uploaded sucess")
+        //     } else {
+        //         message.error(errorMessage)
+        //     }
+
+        // },
+
+        async postContent(payload) {
+
+            let formData = new FormData();
+            const authToken = authHeader();
+            const keys = Object.keys(payload)
+            for (const key of keys) {
+                if (key === 'file' && payload[key] !== undefined) {
+                    console.log(payload)
+                    const files = payload.file
+                    files.map((file: any) => {
+                        formData.append('file', file.originFileObj)
+
+                    })
+
+                } else if (key !== 'file') {
+                    formData.append(key, payload[key])
+                }
+            }
+            console.log(formData.getAll('file'))
+            axios.post("http://8.130.19.187:8083/upload/uploadArticle", formData, { headers: { "Content-Type": "multipart/form-data", ...authToken } }).then((res: any) => {
+                console.log(res)
+                if (res.data.success) {
+                    // this.set(['articles', fromJS(data)]
+                    message.success("uploaded sucess")
+                } else {
+                    message.error(res.data.errorMessage)
+                }
+
+            })
+
+
+
+
+
+        }
+
+
     }),
+
+
 
 
 })
--- src/page/post/index.tsx
+++ src/page/post/index.tsx
@@ -1,12 +1,131 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Form, Input, Button, Upload } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import './index.css'
+import { useModel } from '../../hooks/use-model';
 
 const PostPage = () => {
-    return (
+
+    const [uploaderState, setUploaderState] = useState({
+        previewVisible: false,
+        previewImage: '',
+        previewTitle: '',
+        fileList: [
+
+        ],
+    })
+    const { state, dispatch } = useModel('post')
+    const uploadButton = (
         <div>
-            Post Page
+            <PlusOutlined />
+            <div style={{ marginTop: 8 }}>Upload</div>
         </div>
+    );
+    const validateMessages = {
+        required: '这里还没写呢',
+        types: {
+            email: '${label} is not a valid email!',
+            number: '${label} is not a valid number!',
+        },
+        number: {
+            range: '${label} must be between ${min} and ${max}',
+        },
+    };
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    };
+
+    const onFinish = (values: any) => {
+        dispatch.postContent(values)
+        console.log(values)
+    };
+
+
+
+    return (
+        <div className="post-page">
+            <div className="post-form">
+                <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}
+                    layout="vertical">
+                    <Form.Item name="title" label="帖子叫啥" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="brief" label="一句话概括" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name={"article"} label="帖子内容" rules={[{ required: true }]}>
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item name="file" label="加点图片" getValueFromEvent={normFile}>
+                        <Upload
+                            // action="../assets"
+                            listType="picture-card"
+                            fileList={uploaderState.fileList || []}
+                            onPreview={handlePreview}
+                            onChange={handleChange}
+                            beforeUpload={beforeUploader}
+                        >
+                            {uploaderState.fileList.length >= 8 ? null : uploadButton}
+                        </Upload>
+                    </Form.Item>
+                    <Form.Item name="label" label="加个标签" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+
+
     )
+
+    function getBase64(file: any) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+    }
+    function beforeUploader({ fileList }: any) {
+        //avoid auto uploading
+        return false
+
+    }
+
+
+    function normFile(e: any) {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    };
+
+
+    async function handlePreview(file: any) {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+
+        setUploaderState({
+            ...uploaderState,
+            previewImage: file.url || file.preview,
+            previewVisible: true,
+            previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+        });
+    };
+
+    function handleChange({ fileList }: any) {
+        setUploaderState({ ...uploaderState, fileList })
+        console.log(fileList)
+    };
 }
 
 export default PostPage
